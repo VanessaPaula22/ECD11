b766b6bc1a7ec321f4190979d1b992fae945d77d
VanessaPaula22 <vanessaresmini22@hotmail.com>
2024-10-24 22:55:11 -0300

    Update and rename implementation.py to useful_functions.py
    Update the code only with the functions

---
+++ pipeline/useful_functions.py
@@ -0,0 +1,44 @@
+get_ipython().run_line_magic('matplotlib', 'notebook')
+import numpy as np
+import pickle, gzip
+import mlp
+import matplotlib.pyplot as plt
+import torch
+import time
+import os
+import urllib.request
+
+# Useful functions
+def initalize_weights_relu(size_layer, size_next_layer):
+    np.random.seed(5)
+    # Method presented in "Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classfication"
+    # He et Al. 2015
+    epsilon = np.sqrt(2.0 / (size_layer * size_next_layer) )
+    # Weigts from Normal distribution mean = 0, std = epsion
+    w = epsilon * (np.random.randn(size_next_layer, size_layer))
+    return w.transpose()
+def load_mnist():
+    # Download MNIST data if needed
+    mnist_filename = 'mnist.pkl.gz'
+    if not os.path.exists(mnist_filename):    
+        ulr_mnist = 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
+        urllib.request.urlretrieve(ulr_mnist, mnist_filename)
+    
+    # Import MNIST data
+    with gzip.open('mnist.pkl.gz', 'rb') as f:
+        train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+    # Training data, only
+    X = valid_set[0]
+    y = valid_set[1]
+
+    # change y [1D] to Y [2D] sparse array coding class
+    n_examples = len(y)
+    labels = np.unique(y)
+    Y = np.zeros((n_examples, len(labels)))
+    for ix_label in range(len(labels)):
+        # Find examples with with a Label = lables(ix_label)
+        ix_tmp = np.where(y == labels[ix_label])[0]
+        Y[ix_tmp, ix_label] = 1
+
+    return X, Y, labels, y
+
